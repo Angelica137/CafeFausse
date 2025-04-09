b229921210cbff890101d2373c914edf863c0e67
Angelica <Angelica137@users.noreply.github.com>
2025-04-09 20:43:30 +0100

    add left nav, green

--- restaurant-website/src/components/Header/Header.jsx
+++ restaurant-website/src/components/Header/Header.jsx
@@ -5,6 +5,14 @@ const Header = ({ restaurantName }) => {
   return (
     <header className="header">
 			<div className="header-content">
+				<div className="left-nav">
+					<nav>
+						<ul>
+							<li><a href="/reservations">Make a Reservation</a></li>
+							<li><a href="/menu">Menu</a></li>
+						</ul>
+					</nav>
+				</div>
 				<div className="logo-container">
 					<h1 className="restaurant-name">{restaurantName}</h1>
 				</div>
