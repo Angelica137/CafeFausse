79371362c0f55fb776504beed7d06a983aedfc76
Angelica <Angelica137@users.noreply.github.com>
2025-04-09 20:19:35 +0100

    add logo container, green

--- restaurant-website/src/components/Header/Header.jsx
+++ restaurant-website/src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import "./Header.css";
 const Header = ({ restaurantName }) => {
   return (
     <header className="header">
-      <h1 className="restaurant-name">{restaurantName}</h1>
+			<div className="header-content">
+				<div className="logo-container">
+					<h1 className="restaurant-name">{restaurantName}</h1>
+				</div>
+			</div>
     </header>
   );
 };
